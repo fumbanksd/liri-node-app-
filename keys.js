a8e75cad23da1cb607b10ce63cc400775a8ae438
Dominic Fumbanks <fumbanksd@gmail.com>
2018-02-12 19:20:07 -0500

    message

--- keys.js
+++ keys.js
@@ -1,13 +1,10 @@
 console.log('this is loaded');
 
 exports.twitter = {
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+  consumer_key: 'u5L64CVk3QLvKMfFJjkHoKVsT',
+  consumer_secret: 'DO27nuQRnCyYetIM1cfG01tU46z8yE9PG62ZLgHrmOfQ9oJvOV',
+  access_token_key: '820436163324162054-R02uzHa1i4fB5yKHK7VTzbOmzXFyKFI',
+  access_token_secret: '6ty7k7WipMyxdBYlAkqoIdRRFd6hjw5c34Zhn2uGrm6Dj',
 };
 
-exports.spotify = {
-  id: process.env.SPOTIFY_ID,
-  secret: process.env.SPOTIFY_SECRET
-};
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,157 @@
+require("dotenv").config();
+
+
+var twitter = require('twitter');
+var twitterKeys = require('./keys.js').twitterKeys;
+var T = new twitter(twitterKeys);
+
+
+var params = {
+ screen_name: "bobzm51",
+ count: 20,
+     q: "bobzm51"
+}
+
+function getData() {
+    T.get('search/tweets', params, function(err, data, response) {
+
+var tweets = data.statuses;
+for (var i = 0; i < tweets.length; i++) {
+console.log(tweets[i].text)
+console.log(tweets[i].created_at);
+  }
+ });
+}
+
+
+
+
+
+var spotify = require('spotify');
+
+
+function getSong() {
+spotify.search({ type: 'track', query: command2 }, function(err, data) {
+if (err) {
+console.log('Error occurred: ' + err);
+ return;
+  }
+
+console.log(data.tracks.items);
+
+var items = data.tracks.items;
+
+for (i = 0; i < items.length; i++) {
+console.log(items[i].album.name);
+}
+
+for (i = 0; i < items.length; i++) {
+console.log(items[i].name);
+ }
+
+for (i = 0; i < items.length; i++) {
+console.log(items[i].artists);
+        }
+
+for (i = 0; i < items.length; i++) {
+console.log(items[i].preview_url);
+}
+
+});
+}
+
+
+var request = require('request');
+var queryUrl = 'http://www.omdbapi.com/?t=' + command2 + '&y=&plot=short&r=json';
+
+function getMovie() {
+request(queryUrl, function(error, response, body) {
+if (!error && response.statusCode == 200) {
+console.log(JSON.parse(body)["Released"])
+console.log(JSON.parse(body)["Title"])
+console.log(JSON.parse(body)["imdbRating"])
+console.log(JSON.parse(body)["Country"])
+console.log(JSON.parse(body)["Language"])
+console.log(JSON.parse(body)["Plot"])
+console.log(JSON.parse(body)["Actors"])
+         
+        }
+    });
+}
+
+
+
+var fs = require('fs');
+
+function doIt() {
+fs.readFile("random.txt", "utf8", function(err, data) {
+var output = data.split();
+for (var i = 0; i < output.length; i++) {
+console.log(output[i]);
+        }
+
+function getSong() {
+spotify.search({ type: 'track', query: output[i] }, function(err, data) {
+if (err) {
+console.log('Error occurred: ' + err);
+return;
+ }
+
+console.log(data.tracks.items);
+          var items = data.tracks.items;
+for (i = 0; i < items.length; i++) {
+console.log(items[i].album.name);
+   }
+for (i = 0; i < items.length; i++) {
+console.log(items[i].name);
+   }
+for (i = 0; i < items.length; i++) {
+console.log(items[i].artists);
+   }
+for (i = 0; i < items.length; i++) {
+console.log(items[i].preview_url);
+   }
+
+   });
+     }
+        getSong();
+    });
+}
+
+
+
+
+
+
+var command1 = process.argv[2];
+
+var command2 = "";
+var nodeArg = process.argv;
+
+for (var i = 3; i < nodeArg.length; i++) {
+    command2 = nodeArg[i];
+
+}
+
+
+
+console.log(command2);
+
+
+
+switch (command1 + command2) {
+case "my-tweets":
+getData();
+break;
+case "spotify-this-song" + command2:
+getSong();
+break;
+case "movie-this":
+getMovie();
+break;
+case "do-what-it-says":
+doIt();
+break;
+default:
+console.log("invalid value");
+}
